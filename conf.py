ba38643c4ec7c8dab2b41933dbde9178ef2b6831
jekil <alessandro@tanasi.it>
2014-12-20 11:53:23 +0100

    Moved to alabaster

--- conf.py
+++ conf.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
-import sys, os
+import sys
+import os
+import alabaster
 from datetime import datetime
 
 # If extensions (or modules to document with autodoc) are in another directory,
@@ -15,7 +17,7 @@ from datetime import datetime
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = []
+extensions = ['alabaster']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -85,7 +87,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -93,7 +95,7 @@ html_theme = 'default'
 #html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = []
+html_theme_path = [alabaster.get_path()]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
