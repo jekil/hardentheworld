600300fc5daaae56a13644333cc804313c322b5b
jekil <alessandro@tanasi.it>
2014-12-20 12:32:23 +0100

    Settings

--- conf.py
+++ conf.py
@@ -92,7 +92,19 @@ html_theme = 'alabaster'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+    #'logo': 'logo.png',
+    #'logo_name': True,
+    #'logo_text_align': 'center',
+    'description': "Hardening guidelines",
+    'github_user': 'jekil',
+    'github_repo': 'hardentheworld',
+    'travis_button': False,
+    #'gratipay_user': 'jekil',
+    'analytics_id': 'UA-2317228-21',
+    #'link': '#3782BE',
+    #'link_hover': '#3782BE',
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 html_theme_path = [alabaster.get_path()]
@@ -127,7 +139,14 @@ html_static_path = ['_static']
 #html_use_smartypants = True
 
 # Custom sidebar templates, maps document names to template names.
-#html_sidebars = {}
+html_sidebars = {
+    '**': [
+        'about.html',
+        'navigation.html',
+        'searchbox.html',
+        #'donate.html',
+    ]
+}
 
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
